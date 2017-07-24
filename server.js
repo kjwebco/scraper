e4efb3a5d616640a0813aca605e55d9e6f1d1572
Kevin Jendreas <kjendreas@gmail.com>
2017-07-23 21:36:54 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ var routes = require("./controllers/scraper_controller.js");
 
 app.use("/", routes);
 
-// mongoose.connect("mongodb://heroku_gnzk5747:4d2121nhgnfbdl1pfirsdepk9n@ds125262.mlab.com:25262/heroku_gnzk5747");
 
 var db = mongoose.connection;
 
